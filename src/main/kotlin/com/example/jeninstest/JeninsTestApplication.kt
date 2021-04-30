e38abf0f073057c01d6184eec06495d2f8d59e68
mahmoud <mahmoud.kheyrati.fard@gmail.com>
2021-04-30 17:21:38 +0430

    init

---
+++ src/main/kotlin/com/example/jeninstest/JeninsTestApplication.kt
@@ -0,0 +1,11 @@
+package com.example.jeninstest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JeninsTestApplication
+
+fun main(args: Array<String>) {
+    runApplication<JeninsTestApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/jeninstest/JeninsTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.jeninstest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JeninsTestApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
